d59e0a7708c12e13d8b225210456afbc66160207
sajibms <shahadath.sajib.ms@gmail.com>
2025-01-14 11:58:46 +0600

    feat: added apps two

---
+++ apps/saas-two/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import { vitePlugin as remix } from '@remix-run/dev'
+
+declare module '@remix-run/node' {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
+
+export default defineConfig({
+  root: __dirname,
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    nxViteTsPaths(),
+  ],
+})
