0acc8966f9830e48f474522ee11387fcf77a7006
sajibms <shahadath.sajib.ms@gmail.com>
2025-01-15 13:07:48 +0600

    feat: fixed lint error

--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
 import * as path from 'path'
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: __dirname,
